3ac2b8b2efadf024dfcf58e7ef8341b1a89cf1b1
deepzz0 <deepzz.qi@gmail.com>
2020-11-14 16:22:39 +0800

    feat(docker): make build, build docker image

--- Makefile
+++ Makefile
@@ -1,8 +1,14 @@
-.PHONY: demo
+.PHONY: demo build swag
 
+# run demo app
 demo:
 	@scripts/run_demo.sh
 
+# build docker
+build:
+	@scripts/run_build.sh
+
+# generate swag docs
 swag:
 	@swag init --generalInfo pkg/api/api.go
 
---
+++ scripts/run_build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+set -e
+
+# prepare dir ./bin
+rm -rf ./bin
+mkdir ./bin
+
+# build demo app
+GOOS=linux GOARCH=amd64 go build -o bin/ ./cmd/demo
+docker build -f build/package/demo.Dockerfile -t appdemo:demoapp .
+
+# clean dir ./bin
+rm -rf ./bin
