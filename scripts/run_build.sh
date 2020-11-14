4589df60aaf080fd44455106f72f3844c69a7687
deepzz0 <deepzz.qi@gmail.com>
2020-11-14 17:32:39 +0800

    chore(scripts): update build script

--- scripts/run_build.sh
+++ scripts/run_build.sh
@@ -8,7 +8,7 @@ mkdir ./bin
 
 # build demo app
 GOOS=linux GOARCH=amd64 go build -o bin/ ./cmd/demo
-docker build -f build/package/demo.Dockerfile -t appdemo:demoapp .
+docker build -f build/package/demo.Dockerfile -t appdemo:demo .
 
 # clean dir ./bin
 rm -rf ./bin
